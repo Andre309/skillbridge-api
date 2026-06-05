3bb1ab6dd096857c82b382cc47e3b71fb8b8461c
Andre309 <andrij.kibish@gmail.com>
2026-06-05 16:55:03 +0300

    fix: specify types in tsconfig

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from '@prisma/client'
-
+import { logger } from './logger'
+ 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
-
+ 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient()
-
+ 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma
 }
-
+ 
 export default prisma
